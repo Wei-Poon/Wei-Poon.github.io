5ce302c8ba5816a5ba493159c4ec0a67551941fe
Wei-Poon <64232619+Wei-Poon@users.noreply.github.com>
2020-05-14 22:20:15 +1000

    weirdsignals2

--- weird signals/assets/js/main.js
+++ weird signals/assets/js/main.js
@@ -1,11 +1,29 @@
 $(document).ready(function () {
-  $(".menu").click(function () {
-    $(".essay").addClass("slide");
+  $(".menu1").click(function () {
+    $(".essay1").addClass("slide");
+    $(".essay2").removeClass("slide");
+    $(".essay3").removeClass("slide");
+    $(".close").addClass("show");
+  });
+
+  $(".menu2").click(function () {
+    $(".essay2").addClass("slide");
+    $(".essay1").removeClass("slide");
+    $(".essay3").removeClass("slide");
+    $(".close").addClass("show");
+  });
+
+  $(".menu3").click(function () {
+    $(".essay3").addClass("slide");
+    $(".essay1").removeClass("slide");
+    $(".essay2").removeClass("slide");
     $(".close").addClass("show");
   });
 
   $(".close").click(function () {
-    $(".essay").removeClass("slide");
+    $(".essay1").removeClass("slide");
+    $(".essay2").removeClass("slide");
+    $(".essay3").removeClass("slide");
     $(".close").removeClass("show");
   });
 });
