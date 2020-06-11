235062b68b0c438b350f066ec403edd74f9047d6
Wei-Poon <64232619+Wei-Poon@users.noreply.github.com>
2020-06-12 08:49:26 +1000

    temporal loops added

---
+++ 2045/assets/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	console.log("im ready bruh");
+
+	$('.he1').click(function(){
+		$('.to1').slideToggle("slow");
+	});
+
+	$('.he2').click(function(){
+		$('.to2').slideToggle("slow");
+	});
+
+	$('.he3').click(function(){
+		$('.to3').slideToggle("slow");
+	});
+
+});
