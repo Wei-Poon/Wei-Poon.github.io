8c653f86e390d85d2d7b1721a536027761f0c7d5
Wei-Poon <64232619+Wei-Poon@users.noreply.github.com>
2020-06-26 20:49:27 +1000

    slide in and sidebar

--- temporal-loops/assets/js/main.js
+++ temporal-loops/assets/js/main.js
@@ -1,56 +1,81 @@
 $(document).ready(function(){
 	console.log("im ready bruh");
 
+	// floor slide out
+
+	$('.lv5Ext').click(function(){
+		$('.lv5Ext').addClass("slide");
+	});
+
+	$('.lv4Ext').click(function(){
+		$('.lv4Ext').addClass("slide");
+	});
+
+	$('.lv3Ext').click(function(){
+		$('.lv3Ext').addClass("slide");
+	});
+
+	$('.lv2Ext').click(function(){
+		$('.lv2Ext').addClass("slide");
+	});
+
 	$('.lv1Ext').click(function(){
-		$('.lv1Ext').addClass("slide");
+		$(".lv1Ext").addClass("slide");
 	});
 
-	$('.lv2').click(function(){
-		$('.lv2').addClass("slide");
+	// end floor slide out
+
+	// floor slide in
+
+	$('.lv5Int').click(function(){
+		$(".lv5Ext").removeClass("slide");
 	});
 
-	$('.lv3').click(function(){
-		$('.lv3').addClass("slide");
+	$('.lv4Int').click(function(){
+		$(".lv4Ext").removeClass("slide");
 	});
 
-	$('.lv4').click(function(){
-		$('.lv4').addClass("slide");
+	$('.lv3Int').click(function(){
+		$(".lv3Ext").removeClass("slide");
 	});
 
-
-	$('.lv5').click(function(){
-		$(".lv5").addClass("slide");
+	$('.lv2Int').click(function(){
+		$(".lv2Ext").removeClass("slide");
 	});
 
-	$('.floorInt').click(function(){
+	$('.lv1Int').click(function(){
 		$(".lv1Ext").removeClass("slide");
 	});
+
+	// end floor slide in
+
+	// click to scroll (floating sidebar)
 
   	$('div.flv5').click(function() {
   		$('html, body').animate({
     scrollTop: 
-   	$("div.lv5").offset().top
+   	$("div.lv5").offset().top - 125
   		}, 1000)
 	}), 
  	
  	$('div.flv4').click(function (){
    		$('html, body').animate({
     scrollTop:
-    $("div.lv4").offset().top
+    $("div.lv4").offset().top - 125
     	}, 1000)
   	}),
   		
   	$('div.flv3').click(function (){
     	$('html, body').animate({
     scrollTop:
-    $("div.lv3").offset().top
+    $("div.lv3").offset().top - 125
     	}, 1000)
   	})
 
   	$('div.flv2').click(function (){
     	$('html, body').animate({
     scrollTop:
-    $("div.lv2").offset().top
+    $("div.lv2").offset().top - 125
     	}, 1000)
   	})
 
@@ -60,14 +85,7 @@ $(document).ready(function(){
     $("div.lv1").offset().top
     	}, 1000)
   	})
-	
 
-	// $(".menu1").click(function () {
- //    	$(".essay1").addClass("slide");
- //    	$(".essay2").removeClass("slide");
- //    	$(".essay3").removeClass("slide");
- //    	$(".close").addClass("show");
- //  	});
-
+  	// end click to scroll
 
 });
