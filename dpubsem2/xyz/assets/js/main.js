1aea2d23b8b404daab0a6ebf6a24af311f54e9f2
Wei-Poon <64232619+Wei-Poon@users.noreply.github.com>
2020-08-28 02:06:25 +1000

    xyz pages

--- dpubsem2/xyz/assets/js/main.js
+++ dpubsem2/xyz/assets/js/main.js
@@ -1,31 +1,50 @@
 $(document).ready(function(){
 
 var word1 = [
-	"Tuts my barreh",
-	"ohmygaah",
-	"Datili"
+	"the creak of a door",
+	"a thunderous roar",
+	"a subtle chime",
+	"the restless winds",
+	"a soothing melody"
 ];
 
 var randomWord1 = word1[Math.floor(Math.random()*word1.length)];
 
 var word2 = [
-  "yell",
-  "dance",
-  "draw"
+  "the raging masses",
+  "the lifeless streets",
+  "a blank stare",
+  "the bustling city",
+  "unending reflections"
+
 ];
 
 var randomWord2 = word2[Math.floor(Math.random()*word2.length)];
 
 var word3 = [
-  "red",
-  "blue",
-  "yellow"
+  "crimson, filled with smoke",
+  "empty, no clouds or stars",
+  "on fire",
+  "below you",
+  "falling"
 ];
 
 var randomWord3 = word3[Math.floor(Math.random()*word3.length)];
 
+
+var word4 = [
+  "Welcome to the Afterlife",
+  "Your journey begins here",
+  "Your life is in danger",
+  "Perhaps this is just a dream",
+  "..."
+];
+
+var randomWord4 = word4[Math.floor(Math.random()*word4.length)];
+
 $( ".word1" ).html(randomWord1);
 $( ".word2" ).html(randomWord2);
 $( ".word3" ).html(randomWord3);
+$( ".word4" ).html(randomWord4);
 
 });
