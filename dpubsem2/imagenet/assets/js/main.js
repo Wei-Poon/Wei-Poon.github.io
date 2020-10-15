9ea89bc6f82d77b5753726aeb812a23d190dc1d3
Wei-Poon <64232619+Wei-Poon@users.noreply.github.com>
2020-10-16 04:05:39 +1100

    added all imagenet code

---
+++ dpubsem2/imagenet/assets/js/main.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+	console.log('WATUPPP');
+
+	$('.about').click(function(){
+		$('.about').addClass('hide');
+	});
+
+	$('.btn-short').click(function(){
+		$('.item').addClass('hide');
+		$('.item.short').removeClass('hide');
+	});
+
+	$('.btn-oldold').click(function(){
+		$('.item').addClass('hide');
+		$('.item.oldold').removeClass('hide');
+	});
+
+	$('.btn-old').click(function(){
+		$('.item').addClass('hide');
+		$('.item.old').removeClass('hide');
+	});
+
+	$('.btn-tall').click(function(){
+		$('.item').addClass('hide');
+		$('.item.tall').removeClass('hide');
+	});
+
+	$('.btn-new').click(function(){
+		$('.item').addClass('hide');
+		$('.item.new').removeClass('hide');
+	});
+
+	$('.btn-robot').click(function(){
+		$('.item').addClass('hide');
+		$('.item.robot').removeClass('hide');
+	});
+
+	$('.btn-res').click(function(){
+		$('.item').removeClass('hide');
+	});
+
+	$(function(){
+		$(".container .item").draggable();
+	});
+
+	$(function(){
+		$(".about").resizable();
+	});
+
+});
