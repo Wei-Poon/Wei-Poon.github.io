9304e9a062b7d93651752c6b873ff0a19bea931c
Wei-Poon <64232619+Wei-Poon@users.noreply.github.com>
2020-11-27 02:41:09 +1100

    folio added

---
+++ folio/assets/js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+	console.log('wattup');
+
+	$('.first').click(function(){
+		$('.container.misinfo').slideToggle('hidden');
+	});
+
+	$('.sec').click(function(){
+		$('.container.notabook').slideToggle('hidden');
+	});
+
+	$('.thr').click(function(){
+		$('.container.australis').slideToggle('hidden');
+	});
+
+	$('.for').click(function(){
+		$('.container.dock').slideToggle('hidden');
+	});
+
+	$('.fif').click(function(){
+		$('.container.imagenet').slideToggle('hidden');
+	});
+
+	$('.six').click(function(){
+		$('.container.voyage').slideToggle('hidden');
+	});
+
+
+
+
+
+});
